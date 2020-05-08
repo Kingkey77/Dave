0ae55c2c094acbd1b2ac0d3f15e21ab84ca28c7b
Chewie77 <kieran77walsh@googlemail.com>
2020-05-08 13:43:13 +0100

    Some changes to levels and soundclip usage

--- Assets/Scripts/Dave.cs
+++ Assets/Scripts/Dave.cs
@@ -1,31 +1,36 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Dave : MonoBehaviour
 {
+    [SerializeField] float thrustMultiplier = 1000f;
+    [SerializeField] float rcsThrust = 10f;
+    [SerializeField] AudioClip daveThrustSound;
+
     private Rigidbody rb;
     private AudioSource audioSource;
+
     bool audioClip;
     bool isPlaying;
-    [SerializeField]float thrustMultiplier = 1000f;
-    [SerializeField]float rcsThrust = 10f;
+    
+    enum State { Alive, Dying, Transcending }
+    State state = State.Alive;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
-        //fartPlay = true;
-        //playToggle = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotation();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotation();
+        }
     }
     protected void LateUpdate()         //Added to stop wierd rotations in x and y
     {
@@ -34,57 +39,77 @@ public class Dave : MonoBehaviour
 
     private void Thrust()
     {
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            float thrustThisFrame;
-            thrustThisFrame = thrustMultiplier * Time.deltaTime;
-            rb.AddRelativeForce(Vector3.up * thrustThisFrame);
-            if (!audioSource.isPlaying)
+            if (Input.GetKey(KeyCode.Space))
             {
-                audioSource.Play();
+                float thrustThisFrame;
+                thrustThisFrame = thrustMultiplier * Time.deltaTime;
+                rb.AddRelativeForce(Vector3.up * thrustThisFrame);
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.PlayOneShot(daveThrustSound);
+                }
             }
-        }
-        else
-        {
-            audioSource.Stop();
-        }
+            else
+            {
+                audioSource.Stop();
+            }
+        
     }
 
     private void Rotation()
     {
-        
-        float rotationThisFrame = rcsThrust * Time.deltaTime;
-        rb.freezeRotation = true;       //take maunal control of rotation 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
-        {            
-            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+            float rotationThisFrame = rcsThrust * Time.deltaTime;
+            rb.freezeRotation = true;       //take maunal control of rotation 
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
             {
-                print("Nope");
+                if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+                {
+                    print("Nope");
+                }
+                else if (Input.GetKey(KeyCode.A))
+                {
+                    transform.Rotate(Vector3.forward * rotationThisFrame);
+                }
+                else if (Input.GetKey(KeyCode.D))
+                {
+                    transform.Rotate(-Vector3.forward * rotationThisFrame);
+                }
             }
-            else if (Input.GetKey(KeyCode.A))
-            {
-                transform.Rotate(Vector3.forward * rotationThisFrame);
-            }
-            else if (Input.GetKey(KeyCode.D))
-            {
-                transform.Rotate(-Vector3.forward * rotationThisFrame);
-            }
-        }       
-        rb.freezeRotation = false;      //resume physics conrol of rigidbody  
+            rb.freezeRotation = false;      //resume physics conrol of rigidbody  
+     
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive)
+        {
+            return;
+        }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
-                print("OK");
+                // Do Nothing
+                break;
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f);        //Parameterise this time
                 break;
             default:
-                print("dead");
+                state = State.Dying;
+                Invoke("LoadFirstLevel", 1f);
+               
                 break;
         }
                 
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
     }
 }
